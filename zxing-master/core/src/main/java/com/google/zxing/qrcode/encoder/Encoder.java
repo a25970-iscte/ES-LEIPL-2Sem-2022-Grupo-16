65667734daa0f99bf0bedb37b90171d1deb59273
a25970-iscte <99775183+a25970-iscte@users.noreply.github.com>
2022-05-09 12:12:33 +0100

    Add files via upload

--- zxing-master/core/src/main/java/com/google/zxing/qrcode/encoder/Encoder.java
+++ zxing-master/core/src/main/java/com/google/zxing/qrcode/encoder/Encoder.java
@@ -552,26 +552,27 @@ public final class Encoder {
   static void appendNumericBytes(CharSequence content, BitArray bits) {
     int length = content.length();
     int i = 0;
-    while (i < length) {
-      int num1 = content.charAt(i) - '0';
-      if (i + 2 < length) {
-        // Encode three numeric letters in ten bits.
-        int num2 = content.charAt(i + 1) - '0';
-        int num3 = content.charAt(i + 2) - '0';
-        bits.appendBits(num1 * 100 + num2 * 10 + num3, 10);
-        i += 3;
-      } else if (i + 1 < length) {
-        // Encode two numeric letters in seven bits.
-        int num2 = content.charAt(i + 1) - '0';
-        bits.appendBits(num1 * 10 + num2, 7);
-        i += 2;
-      } else {
-        // Encode one numeric letter in four bits.
-        bits.appendBits(num1, 4);
-        i++;
-      }
-    }
+    bitsChecker(content, bits, length, i);
   }
+
+private static void bitsChecker(CharSequence content, BitArray bits, int length, int i) {
+	while (i < length) {
+		int num1 = content.charAt(i) - '0';
+		if (i + 2 < length) {
+			int num2 = content.charAt(i + 1) - '0';
+			int num3 = content.charAt(i + 2) - '0';
+			bits.appendBits(num1 * 100 + num2 * 10 + num3, 10);
+			i += 3;
+		} else if (i + 1 < length) {
+			int num2 = content.charAt(i + 1) - '0';
+			bits.appendBits(num1 * 10 + num2, 7);
+			i += 2;
+		} else {
+			bits.appendBits(num1, 4);
+			i++;
+		}
+	}
+}
 
   static void appendAlphanumericBytes(CharSequence content, BitArray bits) throws WriterException {
     int length = content.length();
