6bc8ab9ab43b1c202873df2fd6a191e31fb696cc
a25970-iscte <99775183+a25970-iscte@users.noreply.github.com>
2022-05-11 00:49:08 +0100

    so trabalhos

--- zxing-master/core/src/main/java/com/google/zxing/datamatrix/encoder/EncoderContextDimension.java
+++ zxing-master/core/src/main/java/com/google/zxing/datamatrix/encoder/EncoderContextDimension.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.google.zxing.datamatrix.encoder;
 
 
@@ -20,27 +19,5 @@ public class EncoderContextDimension {
 		this.minSize = minSize;
 		this.maxSize = maxSize;
 	}
-=======
-package com.google.zxing.datamatrix.encoder;
 
-
-import com.google.zxing.Dimension;
-
-public class EncoderContextDimension {
-	private Dimension minSize;
-	private Dimension maxSize;
-
-	public Dimension getMinSize() {
-		return minSize;
-	}
-
-	public Dimension getMaxSize() {
-		return maxSize;
-	}
-
-	public void setSizeConstraints(Dimension minSize, Dimension maxSize) {
-		this.minSize = minSize;
-		this.maxSize = maxSize;
-	}
->>>>>>> f5b3f440d3d5c867d0d2bed84fdf6f7534bb2705
 }
